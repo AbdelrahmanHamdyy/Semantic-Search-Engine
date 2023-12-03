f1b19226aebd0acbeebe5c5dc8e96e45ace0344a
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-12-03 15:01:02 +0200

    Use minibatch for kmeans

--- kmeans.py
+++ kmeans.py
@@ -1,23 +1,13 @@
 import pandas as pd
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 
 
 def run_kmeans(data, k):
-    # Apply k-means clustering
-    kmeans = KMeans(n_clusters=k, random_state=42)
+    # Create and fit the MiniBatchKMeans model
+    kmeans = MiniBatchKMeans(n_clusters=k, random_state=42)
     kmeans.fit(data)
 
-    # Get centroids
+    # Get the centroids
     centroids = kmeans.cluster_centers_
-
-    # Each vector is assigned to a cluster
-    # labels = kmeans.labels_
-
-    # print("Centroids:", centroids)
-    # print("Labels:", labels)
-    # print("Length:", len(labels))
-
-    # Now, 'centroids' contains the coordinates of the centroids,
-    # and 'labels' contains the cluster assignments for each vector.
 
     return centroids
