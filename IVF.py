6a6acb48ce90d03a5f47fd75db2f83ed2148c6c2
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-12-10 19:06:13 +0200

    Testing RAM for IVF

--- IVF.py
+++ IVF.py
@@ -24,7 +24,7 @@ class Node:
 
 
 class IVF:
-    def __init__(self, file_path="saved_db.bin", new_db=True):
+    def __init__(self, file_path="saved_db.csv", new_db=True):
         self.data_size = 0
         self.n_clusters = CLUSTERS
         self.n_probe = P
@@ -41,11 +41,7 @@ class IVF:
         return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
 
     def save_vectors(self, rows):
-        with open(self.data_file_path, "a+") as fout:
-            for row in rows:
-                id, embed = row["id"], row["embed"]
-                row_str = f"{id}," + ",".join([str(e) for e in embed])
-                fout.write(f"{row_str}\n")
+        np.savetxt(self.data_file_path, rows, delimiter=',')
 
     def set_number_of_clusters(self):
         if self.data_size == 10000:
@@ -61,30 +57,24 @@ class IVF:
         elif self.data_size == 20000000:
             self.n_clusters = 2048
 
-    def insert_records(self, rows: List[Dict[int, Annotated[List[float], 70]]]):
+    def insert_records(self, rows):
         self.data_size += len(rows)
         self.set_number_of_clusters()
         self.save_vectors(rows)
         self.build_index()
 
     def read_data(self):
-        df = pd.read_csv(self.data_file_path, header=None)
+        self.vectors = np.loadtxt(self.data_file_path, delimiter=',')
 
-        self.vectors = df.iloc[:, 1:].to_numpy()
-
-    def generate_ivf(self):
-        self.inverted_index = {i: [] for i in range(self.n_clusters)}
-        self.centroids_dict = {i: [self.centroids[i], 0, 0]
-                               for i in range(self.n_clusters)}
-
-        # Assign each vector to the nearest centroid
+    def train_ivf(self):
         similarities = []
         norms = np.linalg.norm(self.centroids, axis=1)
         for vector in self.vectors:
             similarities.append(np.dot(self.centroids, vector) / (
                 norms * np.linalg.norm(vector)))
-        assigned_centroids = np.argmax(similarities, axis=1)
+        return np.argmax(similarities, axis=1)
 
+    def populate_index(self, assigned_centroids):
         for vector_id, centroid_idx in enumerate(assigned_centroids):
             self.centroids_dict[centroid_idx][1] += 1
             vec = self.vectors[vector_id]
@@ -94,6 +84,15 @@ class IVF:
         for _, val in self.centroids_dict.items():
             val[2] = sum
             sum += val[1]
+
+    def generate_ivf(self):
+        self.inverted_index = {i: [] for i in range(self.n_clusters)}
+        self.centroids_dict = {i: [self.centroids[i], 0, 0]
+                               for i in range(self.n_clusters)}
+
+        assigned_centroids = self.train_ivf()
+
+        self.populate_index(assigned_centroids)
 
     def save_index(self):
         with open(self.index_file_path, 'wb') as file:
@@ -125,19 +124,62 @@ class IVF:
         return np.memmap(
             self.centroids_file_path, dtype=dtype, mode='r')
 
+    def update_centroids(self, assigned_centroids):
+        # Load or create the memory-mapped array for centroids
+        centroids_dtype = np.dtype(
+            [('values', 'f', D), ('count', 'i'), ('prev_count', 'i')])
+        centroids_memmap = np.memmap(
+            self.centroids_file_path, dtype=centroids_dtype, mode='r+')
+
+        # Update counts in the centroids array
+        for centroid_idx in assigned_centroids:
+            # Find the corresponding centroid in the memory-mapped array
+            centroid_record = centroids_memmap[centroid_idx]
+
+            # Update count and prev_count
+            centroid_record['count'] += 1
+            # Increment prev_count of all centroids before the centroid_idx
+            if centroid_idx > 0:
+                # Use slicing to access the relevant portion of the array
+                previous_centroids = centroids_memmap[:centroid_idx]
+
+                # Increment prev_count for each previous centroid
+                for prev_centroid in previous_centroids:
+                    prev_centroid['prev_count'] += 1
+
+    def update_index(self, index_memmap, ids, vectors, assigned_centroids):
+        for id, vector, assigned_centroid in zip(ids, vectors, assigned_centroids):
+            # Find the corresponding list in the memory-mapped array
+            assigned_list = index_memmap[assigned_centroid]['vector']
+
+            # Add the new vector to the list
+            assigned_list.append((id, vector))
+
     def handle_big_data(self):
         # Define the chunk size (adjust as needed)
         chunk_size = 1000000
 
+        # Create or load the memory-mapped array for the index
+        index_dtype = np.dtype([('id', 'i'), ('vector', 'f', D)])
+
+        index_memmap = np.memmap(self.index_file_path,
+                                 dtype=index_dtype, mode='r+')
+
         # Iterate through chunks
-        for chunk_number, self.vectors in enumerate(pd.read_csv(self.data_file_path, chunksize=chunk_size)):
+        for chunk_number, chunk in enumerate(pd.read_csv(self.data_file_path, chunksize=chunk_size)):
             print(f"Processing Chunk {chunk_number + 1}")
-            # Perform operations on the chunk
+            self.vectors = chunk['vector']
             if chunk_number == 0:
                 self.centroids = run_kmeans2(
                     self.vectors[:500000], k=self.n_clusters)
+            assigned_centroids = self.train_ivf()
 
-            # TODO: Build Index
+            # Update Index
+            self.update_index(
+                index_memmap, chunk['id'], chunk['vector'], assigned_centroids)
+
+            # Update Centroids
+            self.update_centroids(assigned_centroids)
 
             # If you want to stop after processing 5 million rows
             if chunk_number + 1 >= self.iterations:
@@ -150,7 +192,7 @@ class IVF:
         else:
             self.read_data()
             self.centroids = run_kmeans2(
-                self.vectors[:100000] if self.data_size > 100000 else self.vectors, k=self.n_clusters)
+                self.vectors[:500000] if self.data_size > 100000 else self.vectors, k=self.n_clusters)
 
         self.generate_ivf()
 
--- evaluation.py
+++ evaluation.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 from worst_case_implementation import VecDBWorst
+from memory_profiler import memory_usage
 import time
 from dataclasses import dataclass
 from typing import List
@@ -10,7 +11,8 @@ from IVF import IVF
 from IVF_PQ import IVF_PQ
 from PQ import PQ
 AVG_OVERX_ROWS = 10
-DATA_PATH = "saved_db.bin"
+DATA_PATH = "saved_db.csv"
+results = []
 
 
 @dataclass
@@ -21,27 +23,27 @@ class Result:
     actual_ids: List[int]
 
 
-def run_queries(db, np_rows, top_k, num_runs):
+def run_queries(db, query, top_k, actual_ids, num_runs):
+    global results
     results = []
     for _ in range(num_runs):
-        query = np.random.random((1, 70))
-
         tic = time.time()
         db_ids = db.retrive(query, top_k)
         toc = time.time()
         run_time = toc - tic
-
-        tic = time.time()
-        actual_ids = np.argsort(np_rows.dot(query.T).T / (np.linalg.norm(
-            np_rows, axis=1) * np.linalg.norm(query)), axis=1).squeeze().tolist()[::-1]
-        toc = time.time()
-        np_run_time = toc - tic
-
         results.append(Result(run_time, top_k, db_ids, actual_ids))
     return results
 
 
-def eval(results: List[Result]):
+def memory_usage_run_queries(args):
+    global results
+    # This part is added to calcauate the RAM usage
+    mem_before = max(memory_usage())
+    mem = memory_usage(proc=(run_queries, args, {}), interval=1e-3)
+    return results, max(mem) - mem_before
+
+
+def evaluate_result(results: List[Result]):
     # scores are negative. So getting 0 is the best score.
     scores = []
     run_time = []
@@ -60,76 +62,72 @@ def eval(results: List[Result]):
             except:
                 score -= len(res.actual_ids)
         scores.append(score)
-    acc = sum(1 for num in scores if num == 0)
-    return sum(scores) / len(scores), sum(run_time) / len(run_time), acc/len(scores)
+
+    return sum(scores) / len(scores), sum(run_time) / len(run_time)
 
 
 if __name__ == "__main__":
-    # Train PQ
-    # pq_model = PQ(cluster_bits=5, number_of_segments=14, data_length=70)
-    # db = IVF_PQ(data_file_path=DATA_PATH,
-    #             n_clusters=64, n_probe=10, pq=pq_model)
+    rng = np.random.default_rng(50)
+    rng_query = np.random.default_rng(10)
+
     db = IVF()
 
-    print("10k")
-    records_np = np.random.random((10000, 70))
-    records_dict = [{"id": i, "embed": list(row)}
-                    for i, row in enumerate(records_np)]
+    records_np = rng.random((1000000, 70), dtype=np.float32)
     _len = len(records_np)
-    db.insert_records(records_dict)
-    res = run_queries(db, records_np, 5, 10)
-    print(eval(res))
+    db.insert_records(records_np)
 
-    print("100k")
-    records_np = np.concatenate([records_np, np.random.random((90000, 70))])
-    records_dict = [
-        {"id": i + _len, "embed": list(row)} for i, row in enumerate(records_np[_len:])]
-    _len = len(records_np)
-    db.insert_records(records_dict)
-    res = run_queries(db, records_np, 5, 10)
-    print(eval(res))
+    query = rng_query.random((1, 70), dtype=np.float32)
+    actual_ids = np.argsort(records_np.dot(query.T).T / (np.linalg.norm(
+        records_np, axis=1) * np.linalg.norm(query)), axis=1).squeeze().tolist()[::-1]
+    res = run_queries(db, query, 5, actual_ids, 10)
 
-    print("1M")
-    records_np = np.concatenate([records_np, np.random.random((900000, 70))])
-    records_dict = [
-        {"id": i + _len, "embed": list(row)} for i, row in enumerate(records_np[_len:])]
-    _len = len(records_np)
-    db.insert_records(records_dict)
-    res = run_queries(db, records_np, 5, 10)
-    print(eval(res))
+    res, mem = memory_usage_run_queries((db, query, 5, actual_ids, 3))
+    eval = evaluate_result(res)
+    to_print = f"1M\tscore\t{eval[0]}\ttime\t{eval[1]:.2f}\tRAM\t{mem:.2f} MB"
+
+    print(to_print)
+
+    # print("100k")
+    # records_np = np.concatenate([records_np, np.random.random((90000, 70))])
+    # _len = len(records_np)
+    # db.insert_records(records_np)
+    # res = run_queries(db, records_np, 5, 10)
+    # print(evaluate_result(res))
+
+    # print("1M")
+    # records_np = np.concatenate([records_np, np.random.random((900000, 70))])
+    # _len = len(records_np)
+    # db.insert_records(records_np)
+    # res = run_queries(db, records_np, 5, 10)
+    # print(evaluate_result(res))
 
     # print("5M")
     # records_np = np.concatenate([records_np, np.random.random((4000000, 70))])
-    # records_dict = [
-    #     {"id": i + _len, "embed": list(row)} for i, row in enumerate(records_np[_len:])]
     # _len = len(records_np)
-    # db.insert_records(records_dict)
+    # db.insert_records(records_np)
     # res = run_queries(db, records_np, 5, 10)
-    # print(eval(res))
+    # print(evaluate_result(res))
 
     # print("10M")
     # records_np = np.concatenate([records_np, np.random.random((5000000, 70))])
-    # records_dict = [{"id": i + _len, "embed": list(row)} for i, row in enumerate(records_np[_len:])]
     # _len = len(records_np)
-    # db.insert_records(records_dict)
+    # db.insert_records(records_np)
     # res = run_queries(db, records_np, 5, 10)
-    # print(eval(res))
+    # print(evaluate_result(res))
 
     # print("15M")
     # records_np = np.concatenate([records_np, np.random.random((5000000, 70))])
-    # records_dict = [{"id": i +  _len, "embed": list(row)} for i, row in enumerate(records_np[_len:])]
     # _len = len(records_np)
-    # db.insert_records(records_dict)
+    # db.insert_records(records_np)
     # res = run_queries(db, records_np, 5, 10)
-    # print(eval(res))
+    # print(evaluate_result(res))
 
     # print("20M")
     # records_np = np.concatenate([records_np, np.random.random((5000000, 70))])
-    # records_dict = [{"id": i +  _len, "embed": list(row)} for i, row in enumerate(records_np[_len:])]
     # _len = len(records_np)
-    # db.insert_records(records_dict)
+    # db.insert_records(records_np)
     # res = run_queries(db, records_np, 5, 10)
-    # print(eval(res))
+    # print(evaluate_result(res))
 
     if os.path.exists(DATA_PATH):
         os.remove(DATA_PATH)
